13c6244d08809d7f6eeae1c1fe7f40e485549041
jonasfehr <mail@jonasfehr.ch>
2018-08-15 14:25:03 +0200

    first commit

---
+++ addon_config.mk
@@ -0,0 +1,81 @@
+# All variables and this file are optional, if they are not present the PG and the
+# makefiles will try to parse the correct values from the file system.
+#
+# Variables that specify exclusions can use % as a wildcard to specify that anything in
+# that position will match. A partial path can also be specified to, for example, exclude
+# a whole folder from the parsed paths from the file system
+#
+# Variables can be specified using = or +=
+# = will clear the contents of that variable both specified from the file or the ones parsed
+# from the file system
+# += will add the values to the previous ones in the file or the ones parsed from the file
+# system
+#
+# The PG can be used to detect errors in this file, just create a new project with this addon
+# and the PG will write to the console the kind of error and in which line it is
+
+meta:
+	ADDON_NAME = ofxAddonTemplate
+	ADDON_DESCRIPTION = Addon to use openCV installed through Homebrew
+	ADDON_AUTHOR = @jonasfehr
+	ADDON_TAGS = "opencv" "template"
+	ADDON_URL = http://github.com/yournamehere/ofxAddonTemplate
+
+common:
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	# ADDON_DEPENDENCIES =
+
+	# include search paths, this will be usually parsed from the file system
+	# but if the addon or addon libraries need special search paths they can be
+	# specified here separated by spaces or one per line using +=
+	ADDON_INCLUDES = /usr/local/Cellar/opencv/3.4.2/include
+
+	# any special flag that should be passed to the compiler when using this
+	# addon
+	# ADDON_CFLAGS =
+
+	# any special flag that should be passed to the linker when using this
+	# addon, also used for system libraries with -lname
+	ADDON_LDFLAGS = -I/usr/local/Cellar/opencv/3.4.2/include/opencv -I/usr/local/Cellar/opencv/3.4.2/include -L/usr/local/Cellar/opencv/3.4.2/lib -lopencv_stitching -lopencv_superres -lopencv_videostab -lopencv_aruco -lopencv_bgsegm -lopencv_bioinspired -lopencv_ccalib -lopencv_dnn_objdetect -lopencv_dpm -lopencv_face -lopencv_photo -lopencv_fuzzy -lopencv_hfs -lopencv_img_hash -lopencv_line_descriptor -lopencv_optflow -lopencv_reg -lopencv_rgbd -lopencv_saliency -lopencv_stereo -lopencv_structured_light -lopencv_phase_unwrapping -lopencv_surface_matching -lopencv_tracking -lopencv_datasets -lopencv_dnn -lopencv_plot -lopencv_xfeatures2d -lopencv_shape -lopencv_video -lopencv_ml -lopencv_ximgproc -lopencv_calib3d -lopencv_features2d -lopencv_highgui -lopencv_videoio -lopencv_flann -lopencv_xobjdetect -lopencv_imgcodecs -lopencv_objdetect -lopencv_xphoto -lopencv_imgproc -lopencv_core
+
+	# linux only, any library that should be included in the project using
+	# pkg-config
+	# ADDON_PKG_CONFIG_LIBRARIES =
+
+	# osx/iOS only, any framework that should be included in the project
+	# ADDON_FRAMEWORKS =
+
+	# source files, these will be usually parsed from the file system looking
+	# in the src folders in libs and the root of the addon. if your addon needs
+	# to include files in different places or a different set of files per platform
+	# they can be specified here
+	#ADDON_SOURCES = /usr/local/Cellar/opencv/3.4.2/include
+
+	# some addons need resources to be copied to the bin/data folder of the project
+	# specify here any files that need to be copied, you can use wildcards like * and ?
+	# ADDON_DATA =
+
+	# when parsing the file system looking for libraries exclude this for all or
+	# a specific platform
+	# ADDON_LIBS_EXCLUDE =
+
+	ADDON_LIBS = /usr/local/Cellar/opencv/3.4.2/lib
+
+
+linux64:
+	# binary libraries, these will be usually parsed from the file system but some
+	# libraries need to passed to the linker in a specific order/
+	#
+	# For example in the ofxOpenCV addon we do something like this:
+	#
+	# ADDON_LIBS =
+	# ADDON_LIBS += libs/opencv/lib/linuxarmv6l/libopencv_legacy.a
+	# ADDON_LIBS += libs/opencv/lib/linuxarmv6l/libopencv_calib3d.a
+	# ...
+linux:
+win_cb:
+linuxarmv6l:
+linuxarmv7l:
+android/armeabi:
+android/armeabi-v7a:
