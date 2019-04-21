16e599e908c9dc6531972444c0b9b054c8bccca4
jonasfehr <mail@jonasfehr.ch>
2019-04-21 22:14:35 +0200

    opencv 4.1.0_1 working

--- README.md
+++ README.md
@@ -12,21 +12,21 @@ Installation
 2. Use the Project generator to create your project.
 3. Check if the following lines are correctly added to your Xcode Project file
 
-LINKER FLAGS 
+LINKER FLAGS
 
 to create your own list use following line in your terminal:
-pkg-config --cflags --libs /usr/local/Cellar/opencv/3.4.2/lib/pkgconfig/opencv.pc
+pkg-config --cflags --libs /usr/local/Cellar/opencv/4.1.0_1/lib/pkgconfig/opencv4.pc
 
 ```sh
--I/usr/local/Cellar/opencv/4.0.1/include/opencv4/opencv -I/usr/local/Cellar/opencv/4.0.1/include/opencv4 -L/usr/local/Cellar/opencv/4.0.1/lib -lopencv_gapi -lopencv_stitching -lopencv_aruco -lopencv_bgsegm -lopencv_bioinspired -lopencv_ccalib -lopencv_dnn_objdetect -lopencv_dpm -lopencv_face -lopencv_fuzzy -lopencv_hfs -lopencv_img_hash -lopencv_line_descriptor -lopencv_reg -lopencv_rgbd -lopencv_saliency -lopencv_stereo -lopencv_structured_light -lopencv_phase_unwrapping -lopencv_superres -lopencv_optflow -lopencv_surface_matching -lopencv_tracking -lopencv_datasets -lopencv_dnn -lopencv_plot -lopencv_videostab -lopencv_video -lopencv_xfeatures2d -lopencv_shape -lopencv_ml -lopencv_ximgproc -lopencv_xobjdetect -lopencv_objdetect -lopencv_calib3d -lopencv_features2d -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_flann -lopencv_xphoto -lopencv_photo -lopencv_imgproc -lopencv_core
+-I/usr/local/Cellar/opencv/4.1.0_1/include/opencv4/opencv -I/usr/local/Cellar/opencv/4.1.0_1/include/opencv4 -L/usr/local/Cellar/opencv/4.1.0_1/lib -lopencv_gapi -lopencv_stitching -lopencv_aruco -lopencv_bgsegm -lopencv_bioinspired -lopencv_ccalib -lopencv_dnn_objdetect -lopencv_dpm -lopencv_face -lopencv_freetype -lopencv_fuzzy -lopencv_hfs -lopencv_img_hash -lopencv_line_descriptor -lopencv_quality -lopencv_reg -lopencv_rgbd -lopencv_saliency -lopencv_sfm -lopencv_stereo -lopencv_structured_light -lopencv_phase_unwrapping -lopencv_superres -lopencv_optflow -lopencv_surface_matching -lopencv_tracking -lopencv_datasets -lopencv_text -lopencv_dnn -lopencv_plot -lopencv_videostab -lopencv_video -lopencv_xfeatures2d -lopencv_shape -lopencv_ml -lopencv_ximgproc -lopencv_xobjdetect -lopencv_objdetect -lopencv_calib3d -lopencv_features2d -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_flann -lopencv_xphoto -lopencv_photo -lopencv_imgproc -lopencv_core
 ```
 
 HEADER SEARCH PATH
 ```sh
-/usr/local/Cellar/opencv/4.0.1/include
+/usr/local/Cellar/opencv/4.1.0_1/include/opencv4
 ```
 
 LIBRARY SEARCH PATH (!!! THIS ONE NEEDS TO BE ADDED MANUALT)
 ```sh
-/usr/local/Cellar/opencv/4.0.1/lib
+/usr/local/Cellar/opencv/4.1.0_1/lib
 ```
--- addon_config.mk
+++ addon_config.mk
@@ -19,7 +19,7 @@ meta:
 	ADDON_DESCRIPTION = Addon to use openCV installed through Homebrew
 	ADDON_AUTHOR = @jonasfehr
 	ADDON_TAGS = "opencv" "template"
-	ADDON_URL = http://github.com/yournamehere/ofxAddonTemplate
+	ADDON_URL = http://github.com/jonasfehr/ofxOpencvHomebrew
 
 common:
 	# dependencies with other addons, a list of them separated by spaces
@@ -29,7 +29,7 @@ common:
 	# include search paths, this will be usually parsed from the file system
 	# but if the addon or addon libraries need special search paths they can be
 	# specified here separated by spaces or one per line using +=
-	ADDON_INCLUDES = /usr/local/Cellar/opencv/4.0.1/include
+	ADDON_INCLUDES = /usr/local/Cellar/opencv/4.1.0_1/include/opencv4
 
 	# any special flag that should be passed to the compiler when using this
 	# addon
@@ -37,7 +37,7 @@ common:
 
 	# any special flag that should be passed to the linker when using this
 	# addon, also used for system libraries with -lname
-	ADDON_LDFLAGS = -I/usr/local/Cellar/opencv/4.0.1/include/opencv4/opencv -I/usr/local/Cellar/opencv/4.0.1/include/opencv4 -L/usr/local/Cellar/opencv/4.0.1/lib -lopencv_gapi -lopencv_stitching -lopencv_aruco -lopencv_bgsegm -lopencv_bioinspired -lopencv_ccalib -lopencv_dnn_objdetect -lopencv_dpm -lopencv_face -lopencv_fuzzy -lopencv_hfs -lopencv_img_hash -lopencv_line_descriptor -lopencv_reg -lopencv_rgbd -lopencv_saliency -lopencv_stereo -lopencv_structured_light -lopencv_phase_unwrapping -lopencv_superres -lopencv_optflow -lopencv_surface_matching -lopencv_tracking -lopencv_datasets -lopencv_dnn -lopencv_plot -lopencv_videostab -lopencv_video -lopencv_xfeatures2d -lopencv_shape -lopencv_ml -lopencv_ximgproc -lopencv_xobjdetect -lopencv_objdetect -lopencv_calib3d -lopencv_features2d -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_flann -lopencv_xphoto -lopencv_photo -lopencv_imgproc -lopencv_core
+	ADDON_LDFLAGS = -I/usr/local/Cellar/opencv/4.1.0_1/include/opencv4/opencv -I/usr/local/Cellar/opencv/4.1.0_1/include/opencv4 -L/usr/local/Cellar/opencv/4.1.0_1/lib -lopencv_gapi -lopencv_stitching -lopencv_aruco -lopencv_bgsegm -lopencv_bioinspired -lopencv_ccalib -lopencv_dnn_objdetect -lopencv_dpm -lopencv_face -lopencv_freetype -lopencv_fuzzy -lopencv_hfs -lopencv_img_hash -lopencv_line_descriptor -lopencv_quality -lopencv_reg -lopencv_rgbd -lopencv_saliency -lopencv_sfm -lopencv_stereo -lopencv_structured_light -lopencv_phase_unwrapping -lopencv_superres -lopencv_optflow -lopencv_surface_matching -lopencv_tracking -lopencv_datasets -lopencv_text -lopencv_dnn -lopencv_plot -lopencv_videostab -lopencv_video -lopencv_xfeatures2d -lopencv_shape -lopencv_ml -lopencv_ximgproc -lopencv_xobjdetect -lopencv_objdetect -lopencv_calib3d -lopencv_features2d -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_flann -lopencv_xphoto -lopencv_photo -lopencv_imgproc -lopencv_core
 
 	# linux only, any library that should be included in the project using
 	# pkg-config
@@ -50,7 +50,7 @@ common:
 	# in the src folders in libs and the root of the addon. if your addon needs
 	# to include files in different places or a different set of files per platform
 	# they can be specified here
-	#ADDON_SOURCES = /usr/local/Cellar/opencv/3.4.2/include
+	#ADDON_SOURCES = /usr/local/Cellar/opencv/4.1.0_1/include/opencv4
 
 	# some addons need resources to be copied to the bin/data folder of the project
 	# specify here any files that need to be copied, you can use wildcards like * and ?
@@ -60,7 +60,7 @@ common:
 	# a specific platform
 	# ADDON_LIBS_EXCLUDE =
 
-	# ADDON_LIBS = /usr/local/Cellar/opencv/3.4.2/lib
+	# ADDON_LIBS = /usr/local/Cellar/opencv/4.1.0_1/lib
 
 
 linux64:
